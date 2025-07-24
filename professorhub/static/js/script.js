ddfdb033671b26e2dc59dc6e0c5801c933bb0db9
hick-hpe <>
2025-07-24 11:56:41 -0300

    feat: destacar dia atual

--- professorhub/static/js/script.js
+++ professorhub/static/js/script.js
@@ -5,3 +5,14 @@ document.querySelector('#menu-icon').addEventListener('click', () => {
 document.querySelector('#icon-close').addEventListener('click', () => {
     document.body.classList.toggle('sidebar-open');
 });
+
+// destacar dia atual
+const dataAtual = new Date();
+const diasCalendario = document.querySelectorAll('.dia-calendario');
+
+diasCalendario.forEach((dia) => {
+    if (dia.textContent == dataAtual.getDate()) {
+        dia.classList.add('dia-atual');
+    }
+});
+
